0e09db75f4cbcc6d64a1c6e9cea49492341d62f9
kenken64 <bunnyppl@hotmail.com@users.noreply.github.com>
2020-11-19 06:25:06 +0800

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
+import { Todo } from './todo';
+import { v4 as uuidv4 } from 'uuid';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-todoApp';
+  @Input() max: any;
+  form: FormGroup;
+  todoValues = [];
+  tomorrow = new Date(); 
+
+  constructor(private fb: FormBuilder) {
+    this.tomorrow.setDate(this.tomorrow.getDate() + 1);
+   }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      task: this.fb.control('', [Validators.required]),
+      priority: this.fb.control('', [Validators.required]),
+      dueDate: this.fb.control('', [Validators.required]),
+      todos: new FormArray([]),
+    });
+  }
+
+  addTodo(){
+    console.log("add todo");
+    const todosWithChkArray: FormArray = this.form.get('todos') as FormArray;
+    console.log(this.form.value.task);
+    let taskId = uuidv4();
+    let singleTodo = new Todo(this.form.value.task,
+      this.form.value.priority,
+      this.form.value.dueDate,
+      taskId)
+    this.todoValues.push(singleTodo);
+    const todoGroup = this.fb.group({
+      task: this.fb.control(false)
+    })
+    todosWithChkArray.push(todoGroup);
+    this.form.get('task').reset();
+    this.form.get('priority').reset();
+    this.form.get('dueDate').reset();
+    localStorage.setItem(taskId,JSON.stringify(singleTodo));
+  }
+
+  onDelete(index, taskId){
+    const todosWithChkArray: FormArray = this.form.get('todos') as FormArray;
+    console.log(index);
+    console.log(taskId);
+    todosWithChkArray.removeAt(index)
+    const tobeDeleteTaskIdx = this.todoValues.indexOf(index, 0);
+    if (index > -1) {
+      this.todoValues.splice(tobeDeleteTaskIdx, 1);
+    }
+    localStorage.removeItem(taskId);
+  }
+
+  onUpdate(index, taskId){
+
+  }
+
+  updateTodoStatus(index){
+    this.todoValues[index].status = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'en-SG'},
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS]
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
